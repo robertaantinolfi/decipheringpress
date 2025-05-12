9a2774a9ed8b08a78b684277bcb07919f850d8f7
Roberta Antinolfi <131576108+robertaantinolfi@users.noreply.github.com>
2025-05-12 09:33:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,16 @@
 function toggleAbout() {
 document.getElementById('about').classList.toggle('hidden');
 }
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
